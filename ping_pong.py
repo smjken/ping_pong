7f459ce0fa605445342c15971acb4aaa9606cde2
smjken <134795704+smjken@users.noreply.github.com>
2023-06-03 18:23:00 +0300

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -3,7 +3,34 @@ from pygame import *
 window = display.set_mode((600, 600))
 back = (221, 212, 111)
 window.fill(back)
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height ):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+    def reset(self):
+         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def updat_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > -50:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 520:
+            self.rect.y += self.speed 
+    def updat_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > -50:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 520:
+            self.rect.y += self.speed 
 
 clock = time.Clock()
 display.update()
 clock.tick(60)
+
